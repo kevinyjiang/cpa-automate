0390544acd4c10f35765fde9ef394e4580744b65
kevinyjiang <kyj2108@columbia.edu>
2018-09-20 14:14:50 -0400

    Add logging and create DocumentUI class

--- cpa-ui.py
+++ cpa-ui.py
@@ -1,78 +1,118 @@
-from InvoiceModel import InvoiceModel
+import logging
+
+# Set up logging
+logger = logging.getLogger("main")
+logger.setLevel(logging.DEBUG)
+
+formatString = "%(asctime)s %(levelname)-8.8s [%(module)s] [%(funcName)s:%(lineno)4s] %(message)s"
+formatter = logging.Formatter(formatString)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+fh = logging.FileHandler("log.txt")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+import traceback
+from datetime import datetime
+from validate_email import validate_email
+
 from ModelToPDFConverter import ModelToPDFConverter
+from InvoiceModel import InvoiceModel
 
 from tkinter import *
-fields = 'Invoice Number', 'Invoice Name', 'Invoice Date', 'Job Date', 'Job Location', 'Client Name', 'Client Email', 'Client Phone', 'Photographer Name', 'Event Quantity', 'Portrait Quantity', 'Photobooth Quantity'
+fields = "Invoice Number", "Invoice Name", "Invoice Date", "Job Date", "Job Location", "Client Name", "Client Email", "Client Phone", "Photographer Name", "Event Hours", "Portrait Hours", "Photobooth Hours"
 
-def create_form(root, fields):
-	entries = []
-	for field in fields:
-		row = Frame(root)
-		label = Label(row, width=15, text=field, anchor='w')
-		entry = Entry(row)
-		row.pack(side=TOP, fill=X, padx=5, pady=5)
-		label.pack(side=LEFT)
-		entry.pack(side=RIGHT, expand=YES, fill=X)
-		entries.append((field, entry))
-	return entries
+class DocumentUI:
+	def __init__(self):
+		self.root = Tk()
+		self.root.title("Kyeongjoo SUcks")
 
-def toCamelCase(string): 
-	result = ''.join(x for x in string.title() if not x.isspace()) 
-	return result[0].lower() + result[1:]
+		self.modelToPDFConverter = ModelToPDFConverter()
 
-def generate_docs(entries, invoice, release, whatToExpect):
-	modelToPDFConverter = ModelToPDFConverter()
-	model = InvoiceModel()
+		self.entries = self.create_form(fields)
 
-	for entry in entries:
-		setattr(model, toCamelCase(entry[0]), entry[1].get())
+		self.invoice = IntVar(value=1)
+		self.release = IntVar(value=1)
+		self.whatToExpect = IntVar(value=1)
 
-	eventQuantity = entries[9][1].get()
-	portraitQuantity = entries[10][1].get()
-	photoboothQuantity = entries[11][1].get()
+		self.invoiceButton = Checkbutton(self.root, text="Invoice", 
+			variable=self.invoice, onvalue=1, offvalue=0)
+		self.releaseButton = Checkbutton(self.root, text="Release", 
+			variable=self.release, onvalue=1, offvalue=0)
+		self.whatToExpectButton = Checkbutton(self.root, text="What to Expect", 
+			variable=self.whatToExpect, onvalue=1, offvalue=0)
+		self.invoiceButton.pack(side=LEFT, padx=5, pady=5)
+		self.releaseButton.pack(side=LEFT, padx=5, pady=5)
+		self.whatToExpectButton.pack(side=LEFT, padx=5, pady=5)
 
-	if eventQuantity != '0' and eventQuantity != '':
-		model.addItem("Event Photography", int(eventQuantity))
+		self.goButton = Button(self.root, text="Go", command=self.generate_docs)
+		self.goButton.pack(side=LEFT, padx=5, pady=5)
+		self.quitButton = Button(self.root, text="Quit", command=self.destructor)
+		self.quitButton.pack(side=RIGHT, padx=5, pady=5)
 
-	if portraitQuantity != '0' and portraitQuantity != '': 
-		model.addItem("Portraiture", int(portraitQuantity))
+		self.root.mainloop()
 
-	if photoboothQuantity != '0' and photoboothQuantity != '': 
-		model.addItem("Photobooth", int(photoboothQuantity))
+	def destructor(self):
+		logging.info("Shutting down...")
+		self.root.destroy()
 
-	if invoice:
-		modelToPDFConverter.createInvoice(model)	
-	if release:
-		modelToPDFConverter.createRelease(model)
-	if whatToExpect:
-		modelToPDFConverter.createWhatToExpect(model)
+	def create_form(self, fields):
+		entries = []
+		for field in fields:
+			row = Frame(self.root)
+			label = Label(row, width=15, text=field, anchor="w")
+			entry = Entry(row)
+			row.pack(side=TOP, fill=X, padx=5, pady=5)
+			label.pack(side=LEFT)
+			entry.pack(side=RIGHT, expand=YES, fill=X)
+			entries.append((field, entry))
+		return entries
 
+	def generate_docs(self):
+		logger.info("Go button pressed...")
+		model = InvoiceModel()
+
+		for entry in self.entries:
+			setattr(model, self.toCamelCase(entry[0]), entry[1].get())
+
+		eventQuantity = self.entries[9][1].get()
+		portraitQuantity = self.entries[10][1].get()
+		photoboothQuantity = self.entries[11][1].get()
+
+		if eventQuantity != "0" and eventQuantity != "":
+			model.addItem("Event Photography", int(eventQuantity))
+
+		if portraitQuantity != "0" and portraitQuantity != "": 
+			model.addItem("Portraiture", int(portraitQuantity))
+
+		if photoboothQuantity != "0" and photoboothQuantity != "": 
+			model.addItem("Photobooth", int(photoboothQuantity))
+
+		try:
+			if self.invoice.get():
+				self.modelToPDFConverter.createInvoice(model)	
+			if self.release.get():
+				self.modelToPDFConverter.createRelease(model)
+			if self.whatToExpect.get():
+				self.modelToPDFConverter.createWhatToExpect(model)
+		except:
+			errorMsg = traceback.format_exc()
+			logger.error("[PDF Conversion Error] %s", errorMsg)
+
+	def toCamelCase(self, string): 
+		result = "".join(x for x in string.title() if not x.isspace()) 
+		return result[0].lower() + result[1:]
 
 if __name__ == "__main__":
-	root = Tk()
-	root.title("Kyeongjoo SUcks")
-
-	entries = create_form(root, fields)
-
-	invoice = IntVar(value=1)
-	release = IntVar(value=1)
-	whatToExpect = IntVar(value=1)
-
-	invoiceButton = Checkbutton(root, text = "Invoice", variable = invoice, 
-					onvalue = 1, offvalue = 0)
-	releaseButton = Checkbutton(root, text = "Release", variable = release,
-					onvalue = 1, offvalue = 0)
-	whatToExpectButton = Checkbutton(root, text = "What to Expect", variable = whatToExpect,
-					onvalue = 1, offvalue = 0)
-	invoiceButton.pack(side=LEFT, padx=5, pady=5)
-	releaseButton.pack(side=LEFT, padx=5, pady=5)
-	whatToExpectButton.pack(side=LEFT, padx=5, pady=5)
-
-	goButton = Button(root, text='Go', command=lambda: generate_docs(entries, 
-		invoice.get(), release.get(), whatToExpect.get()))
-	goButton.pack(side=LEFT, padx=5, pady=5)
-	quitButton = Button(root, text='Quit', command=root.quit)
-	quitButton.pack(side=RIGHT, padx=5, pady=5)
-
-	root.mainloop()
+	logger.info("Starting program")
+	try:
+		main = DocumentUI()
+	except:
+		fatalMessage = traceback.format_exc()
+		logger.error("[Unknown Error] %s", fatalMessage)
+		logger.info("Ending Script Unsuccessfully")
 
